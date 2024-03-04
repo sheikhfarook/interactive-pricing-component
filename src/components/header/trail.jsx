e751d37e368ca0eb5b16bff420d2f747f316c6d0
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-04 11:03:25 +0530

    including the code

--- src/components/header/trail.jsx
+++ src/components/header/trail.jsx
@@ -2,8 +2,8 @@ const Trail = () => {
   return (
     <div className="circle_ring">
       <div>
-        {/* <div>Simple, traffic-based pricing</div>
-        <div>Sign-up for our 30-day trial. No credit card required. </div> */}
+        <div>Simple, traffic-based pricing</div>
+        <div>Sign-up for our 30-day trial. No credit card required. </div>
       </div>
     </div>
   );
