43a397566402b0e22c9691257966bfff17f8d1cd
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-04 11:20:50 +0530

    styling the bg images

--- src/components/header/trail.jsx
+++ src/components/header/trail.jsx
@@ -1,9 +1,13 @@
 const Trail = () => {
   return (
     <div className="circle_ring">
-      <div>
-        <div>Simple, traffic-based pricing</div>
-        <div>Sign-up for our 30-day trial. No credit card required. </div>
+      <div className="items-center justify-center">
+        <div className="w-[394px] h-[38px] m-auto text-[#293356] text-[28px] font-[800]  ">
+          Simple, traffic-based pricing
+        </div>
+        <div className="w-[370px] h-[21px] text-[15px] font-[600] m-auto text-[#848ead]">
+          Sign-up for our 30-day trial. No credit card required.{" "}
+        </div>
       </div>
     </div>
   );
