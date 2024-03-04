32569a2563349a653072543680d92149294c83d9
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-04 11:02:55 +0530

    create the trail.jsx file

---
+++ src/components/header/trail.jsx
@@ -0,0 +1,12 @@
+const Trail = () => {
+  return (
+    <div className="circle_ring">
+      <div>
+        {/* <div>Simple, traffic-based pricing</div>
+        <div>Sign-up for our 30-day trial. No credit card required. </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Trail;
