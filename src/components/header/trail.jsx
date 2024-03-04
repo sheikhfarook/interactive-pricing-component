51e123e3ece7fffe3a312eaf25fd03d81f8e76bf
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-04 12:13:45 +0530

    complete the header part

--- src/components/header/trail.jsx
+++ src/components/header/trail.jsx
@@ -1,7 +1,8 @@
+import circle from "../../assets/pattern-circles.svg";
 const Trail = () => {
   return (
     <div className="circle_ring">
-      <div className="pt-10 font-manrope max-sm:mt-20  space-y-2 md:m-20">
+      <div className="pt-10 font-manrope max-sm:pt-20 space-y-2 md:p-20">
         <div
           className="w-[394px] h-[38px] m-auto  text-[#293356] text-[28px] font-[800]
         max-sm:w-[281px] max-sm:h-[27.32px] max-sm:text-[20px]   ">
@@ -11,6 +12,9 @@ const Trail = () => {
           className="w-[370px] h-[21px] text-[15px] font-[600] m-auto text-[#848ead]
         max-sm:w-[180px]  max-sm:h-[46px] max-sm:text-[13px] max-sm:leading-[23px] max-sm:text-center ">
           Sign-up for our 30-day trial. No credit card required.{" "}
+        </div>
+        <div>
+          <img className="m-auto mt-[-6rem]" src={circle} alt="" />
         </div>
       </div>
     </div>
