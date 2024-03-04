30793824f852146ac1251c6f367223771430bbd2
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-03-04 11:41:20 +0530

    complete the all device responsive

--- src/components/header/trail.jsx
+++ src/components/header/trail.jsx
@@ -1,11 +1,15 @@
 const Trail = () => {
   return (
     <div className="circle_ring">
-      <div className="items-center justify-center">
-        <div className="w-[394px] h-[38px] m-auto text-[#293356] text-[28px] font-[800]  ">
+      <div className="pt-10 font-manrope max-sm:mt-20  space-y-2 md:m-20">
+        <div
+          className="w-[394px] h-[38px] m-auto  text-[#293356] text-[28px] font-[800]
+        max-sm:w-[281px] max-sm:h-[27.32px] max-sm:text-[20px]   ">
           Simple, traffic-based pricing
         </div>
-        <div className="w-[370px] h-[21px] text-[15px] font-[600] m-auto text-[#848ead]">
+        <div
+          className="w-[370px] h-[21px] text-[15px] font-[600] m-auto text-[#848ead]
+        max-sm:w-[180px]  max-sm:h-[46px] max-sm:text-[13px] max-sm:leading-[23px] max-sm:text-center ">
           Sign-up for our 30-day trial. No credit card required.{" "}
         </div>
       </div>
